bdc2e4bec6331dfff710f4907152b9470354cdb9
Patrick Meyer <patrick.meyer1992@gmail.com>
2022-09-18 11:43:04 +0200

    Script geschrieben, das detaillierte Informationen zu einer film id herunterlädt. Bisher geladene Informationen gespeichert in /data/json

---
+++ scripts/getMovieDetailInfo.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 17 18:26:31 2022
+
+@author: Patrick
+"""
+import http.client
+import json
+import psycopg2
+import time
+import os
+
+# Intialize
+apiKey = input("Enter apiKey: \n")
+targetFolder = input("Choose target folder: \n")
+
+
+def saveMovieInfo(id, apiKey, targetFolder):
+    """ 
+    Downloads movie info via apiKey, converts it to json and saves to
+    targetFolder
+    """
+    
+    conn = http.client.HTTPSConnection("movie-details1.p.rapidapi.com")
+
+    headers = {
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': "movie-details1.p.rapidapi.com"
+        }
+
+    conn.request("GET", "/imdb_api/movie?id=" + id, headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    with open(targetFolder + "\\" + id + ".json", "w", encoding="utf-8") as f:
+        f.write(data.decode("utf-8"))
+
+
+
+def loadFromJson(id, targetFolder):
+    """
+    Reads a json file at the given loaction.
+    Returns a dictionary
+
+    """
+    data = open(targetFolder + "\\" + id + ".json")
+    return json.load(data)
+    
+
+# Connect to database
+con = psycopg2.connect(
+    host="lichthaus.ddns.net",
+    port = "54320",
+    database="lichthaus",
+    user="donkey",
+    password="axt")
+
+# Get all ids in film
+cur = con.cursor()
+cur.execute("SELECT id FROM film")
+rows = cur.fetchall()
+rows = [item[0] for item in rows]
+
+
+# Keep only the ids where there is no .json file saved for
+existingIDs = os.listdir(targetFolder)
+existingIDs = [f.replace(".json", "") for f in existingIDs]
+newIDs = list(set(rows) - set(existingIDs))
+
+
+# Update the .json files with new ids
+for id in newIDs:
+    print(id)
+    time.sleep(0.25)
+    try:
+        saveMovieInfo(id, apiKey, targetFolder)
+    except Exception as e:
+        print("Problem with: " + id)
+        print(e)
+    
+
