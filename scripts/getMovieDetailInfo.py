8f93c781248e701f35e37e6ffd453782cae12d36
Patrick Meyer <patrick.meyer1992@gmail.com>
2022-10-08 14:21:08 +0200

    getMovieDetailInfo.py neu formatiert

--- scripts/getMovieDetailInfo.py
+++ scripts/getMovieDetailInfo.py
@@ -10,12 +10,12 @@ import psycopg2
 import time
 import os
 
-# Intialize
-apiKey = input("Enter apiKey: \n")
-targetFolder = input("Choose target folder: \n")
+#%% Intialize
+apiKey = open("secrets.txt", "r").readlines()[0].replace("\n", "")
+#targetFolder = input("Choose target folder: \n")
 
 
-def saveMovieInfo(id, apiKey, targetFolder):
+def saveMovieInfo(id, apiKey):
     """ 
     Downloads movie info via apiKey, converts it to json and saves to
     targetFolder
@@ -33,7 +33,7 @@ def saveMovieInfo(id, apiKey, targetFolder):
     res = conn.getresponse()
     data = res.read()
 
-    with open(targetFolder + "\\" + id + ".json", "w", encoding="utf-8") as f:
+    with open("data\\json\\" + id + ".json", "w", encoding="utf-8") as f:
         f.write(data.decode("utf-8"))
 
 
@@ -41,7 +41,7 @@ def saveMovieInfo(id, apiKey, targetFolder):
 
     
 
-# Connect to database
+#%% Connect to database
 con = psycopg2.connect(
     host="lichthaus.ddns.net",
     port = "54320",
@@ -49,25 +49,25 @@ con = psycopg2.connect(
     user="donkey",
     password="axt")
 
-# Get all ids in film
+#%% Get all ids in film
 cur = con.cursor()
 cur.execute("SELECT id FROM film")
 rows = cur.fetchall()
 rows = [item[0] for item in rows]
 
 
-# Keep only the ids where there is no .json file saved for
-existingIDs = os.listdir(targetFolder)
+#%% Keep only the ids where there is no .json file saved for
+existingIDs = os.listdir("data\\json")
 existingIDs = [f.replace(".json", "") for f in existingIDs]
 newIDs = list(set(rows) - set(existingIDs))
 
 
-# Update the .json files with new ids
+#%% Update the .json files with new ids
 for id in newIDs:
     print(id)
     time.sleep(0.25)
     try:
-        saveMovieInfo(id, apiKey, targetFolder)
+        saveMovieInfo(id, apiKey)
     except Exception as e:
         print("Problem with: " + id)
         print(e)
